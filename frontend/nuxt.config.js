7c7c3905ca90c7a01e378f94fd9929d674d523b1
Alexander Rybakov <rybakov_web@outlook.com>
2023-11-14 23:10:29 +0300

    run docker

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -50,7 +50,7 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     '@nuxtjs/eslint-module',
-    '~/scroll'
+    '@/scroll'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
