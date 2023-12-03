95f9a6b525c57174d873319c69313d19b8052702
Alexander Rybakov <rybakov_web@outlook.com>
2023-12-04 01:24:38 +0300

    push cloud fix

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -27,7 +27,7 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(raw_password)
         user.save()
         Token.objects.create(user=user)
-        user.send_registration_email(raw_password)
+        user.send_registration_email(name=user, raw_password)
 
         return user
 
